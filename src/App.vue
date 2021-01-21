e93803b7015fb1e4b5a3d084618e4847b6c89ad6
Twizeyimana Elissa <tmanelissa250@gmail.com>
2021-01-21 20:59:48 +0200

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
   <h3>{{title}}</h3>
+  <Navbar/>
   </div>
 </template>
 
 <script>
+
+import Navbar from './Navbar'
 export default {
   name: 'app',
+  components:{
+ Navbar
+  },
   data () {
     return {
      title:" Real time app"
---
+++ src/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav>
+        <ul>
+            <li><a href="www.facebook.com">Facebook</a></li>
+            <li><a href="www.twitter.com">Twitter</a></li>
+            <li><a href="www.linkedin.com">Linkedin</a></li>
+
+        </ul>
+    </nav>
+</template>
+<script>
+export default {
+    el:'Navbar',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style>
+nav{
+    background-color: rgb(129, 129, 167);
+    text-align: center;
+    line-height: 70px;
+}
+nav ul{
+padding: 0;
+ 
+}
+nav li{
+    margin: 5px;
+    color: rgb(236, 236, 241);
+    display: inline-block;
+    font-size: 20px;
+    text-decoration: none;
+    
+}
+
+</style>
